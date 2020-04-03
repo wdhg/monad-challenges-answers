ff0e306c2e9869cd6ffa6b60d67035d41f5d6ece
wdhg <wdhgrant@gmail.com>
2020-04-03 01:22:48 +0100

    add randLetter

--- Set1.hs
+++ Set1.hs
@@ -10,8 +10,15 @@ fiveRands :: [Integer]
 fiveRands
   = [r1, r2, r3, r4, r5]
     where
-      (r1, s1) = rand $ mkSeed 1
+      s0       = mkSeed 1
+      (r1, s1) = rand s0
       (r2, s2) = rand s1
       (r3, s3) = rand s2
       (r4, s4) = rand s3
       (r5, _)  = rand s4
+
+randLetter :: Seed -> (Char, Seed)
+randLetter seed
+  = (toLetter r, seed')
+    where
+      (r, seed') = rand seed
