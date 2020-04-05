31a1a1b4619a73f490b93e76d2542a83f92761c7
wdhg <wdhgrant@gmail.com>
2020-04-05 01:59:30 +0100

    add common Maybe functions

--- Set2.hs
+++ Set2.hs
@@ -5,6 +5,8 @@ module Set2 where
 
 import MCPrelude
 
+-- ex 2 - 1
+
 data Maybe a
   = Just a
   | Nothing
@@ -22,3 +24,45 @@ instance Eq a => Eq (Maybe a) where
     = True
   _ == _
     = False
+
+-- ex 2 - 2
+
+headMay :: [a] -> Maybe a
+headMay (x : xs)
+  = Just x
+headMay _
+  = Nothing
+
+tailMay :: [a] -> Maybe [a]
+tailMay (_ : xs)
+  = Just xs
+tailMay _
+  = Nothing
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay key table
+  = case filter ((== key) . fst) table of
+      ((_, v) : _) -> Just v
+      _            -> Nothing
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay _ 0
+  = Nothing
+divMay x y
+  = Just $ x / y
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay []
+  = Nothing
+maximumMay (x : xs)
+  = case maximumMay xs of
+      Just y  -> Just $ max x y
+      Nothing -> Just x
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay []
+  = Nothing
+minimumMay (x : xs)
+  = case minimumMay xs of
+      Just y  -> Just $ min x y
+      Nothing -> Just x
