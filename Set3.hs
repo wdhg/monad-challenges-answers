32e7ae200a49d43348629d7afca995a8e3fe2935
wdhg <wdhgrant@gmail.com>
2020-04-24 13:24:26 +0100

    add Card data type

--- Set3.hs
+++ Set3.hs
@@ -5,6 +5,13 @@ module Set3 where
 
 import MCPrelude
 
+data Card
+  = Card Int String
+
+instance Show Card where
+  show (Card value suit)
+    = (show value) ++ suit
+
 allPairs :: [a] -> [b] -> [(a, b)]
 allPairs [] _
   = []
