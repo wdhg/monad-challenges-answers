b42c5c5bac57f79df9553c1e285a332a2cac469d
wdhg <wdhgrant@gmail.com>
2020-05-05 22:30:17 +0100

    add newtype Gen

--- Set4.hs
+++ Set4.hs
@@ -4,7 +4,13 @@
 module Set4 where
 
 import MCPrelude
+import Set2
 
 class Monad m where
   bind :: m a -> (a -> m b) -> m b
   return :: a -> m a
+
+newtype Gen a
+  = Gen
+  { runGen :: Seed -> (a, Seed)
+  }
